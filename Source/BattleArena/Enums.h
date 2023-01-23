5dfe72a88a8cf19205c25bfbd525e431647c634c
Jmaidens <jacobmaidens16@gmail.com>
2023-01-23 20:23:31 +0000

    Started weapon stuff
    - Created weapon class
    - Created melle weapon class derived from base weapon
    - Created weapon stats struct
    - Created enums file to store all project enums

---
+++ Source/BattleArena/Enums.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class EWeaponState : uint8
+{
+	WEAPONSTATE_Equipping UMETA(DisplayName = "Equipping"),
+	WEAPONSTATE_Attacking UMETA(DisplayName = "Attacking"),
+	WEAPONSTATE_ReadyAttack UMETA(DisplayName = "ReadyAttack"),
+	WEAPONSTATE_Idle UMETA(DisplayName = "Idle"),
+};
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+	NONE UMETA(DisplayName = "None"),
+	SWORD UMETA(DisplayName = "Sword"),
+	DAGGER UMETA(DisplayName = "Dagger"),
+	AXE UMETA(DisplayName = "Axe"),
+};
